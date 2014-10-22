99ffaa44ce8b49c247c295645adf95ab6bd3ceb0
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2014-10-22 22:30:58 +0700

    change transfer money to function

--- main.go
+++ main.go
@@ -9,9 +9,8 @@ import (
 func main() {
 	source := model.NewAccount("Weerasak", 1000)
 	dest := model.NewAccount("Kanokon", 1000)
-	moneyTransfer := context.NewMoneyTransfer(source, dest)
 
-	moneyTransfer.Execute(500)
+	context.TransferMoney(source, dest, 500.0)
 
 	fmt.Println("Weerasak balance ", source.Balance())
 	fmt.Println("Kanokon balance ", dest.Balance())
